46fd7b4713f8311369e934747891ae8d0439ebb6
David Kuo <s50407s@gmail.com>
2015-08-16 16:33:31 +0800

    對!

--- ivereadthat/read.js
+++ ivereadthat/read.js
@@ -2,18 +2,10 @@ MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 var observer = new MutationObserver(function(mutations, observer) {
     jQuery(".UFILikeLink").each(function() {
         if(this.dataset.ft == '{"tn":">"}') {
-            $(this).html("朕知道了")
+            $(this).html("對")
         }
         else if(this.dataset.ft == '{"tn":"?"}') {
-            $(this).html("朕也是看報紙才知道")
-        }
-    })
-    jQuery(".comment_link > span").each(function() {
-		$(this).text("卿家可知？")
-    })
-    jQuery(".share_action_link").each(function() {
-        if ($(this).text() == "Share" || $(this).text() == "分享") {
-            $(this).text("宣旨")
+            $(this).html("不對")
         }
     })
 })
