7f24cff24237f935b85579580f128b773e1a5ad1
jxcodetw <jxcode.tw@gmail.com>
2015-08-18 02:30:30 +0800

    保留讚的icon

--- ivereadthat/read.js
+++ ivereadthat/read.js
@@ -2,10 +2,10 @@ MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 var observer = new MutationObserver(function(mutations, observer) {
     jQuery(".UFILikeLink").each(function() {
         if(this.dataset.ft == '{"tn":">"}') {
-            $(this).html("對")
+            $(this).find('span').html("對")
         }
         else if(this.dataset.ft == '{"tn":"?"}') {
-            $(this).html("不對")
+            $(this).find('span').html("不對")
         }
     })
 })
